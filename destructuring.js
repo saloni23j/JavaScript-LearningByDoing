a27927b73b00fc24726b60e5887dda5afe5f846f
saloni23j <55657758+saloni23j@users.noreply.github.com>
2021-09-02 23:36:27 +0530

    destructing

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+var suspects = [
+    {
+        name:"Rusty",
+        color : "orange"
+    },
+    {
+        name: "Miss Scarlet",
+        color: "red"
+    }
+]
+var [{color: firstColor}, {color : secondColor}] = suspects;
+console.log(firstColor +" "+ secondColor);
