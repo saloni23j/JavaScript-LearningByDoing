664b762eaad295894a98f21d6b760bb2609660b9
saloni23j <55657758+saloni23j@users.noreply.github.com>
2021-09-02 20:37:11 +0530

    exampleTwo

--- exampleOne.js
+++ exampleOne.js
@@ -10,6 +10,19 @@ const game = {
         }
     ]
 }
+var gameLoop = function(){
+    for(let i = 0; i < game.suspects.length; i++){
+        for(let key in game.suspects[i]){
+            if(game.suspects[i][key] === "Rusty"){
+                console.log("found")
+            }
+            else{
+                console.log("didn't found")
+            }
+        }
+    }
+}
+gameLoop();
 function foo(){
     for(let i = 0; i < game.suspects.length ; i++){
         console.log(game.suspects[i]);
